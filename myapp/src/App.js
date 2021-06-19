c57cc206bddf4152f5afa425e3470ec8aa5fe52a
biaksh-dash <vikashdash004@gmail.com>
2021-06-20 00:01:39 +0530

    map list data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,19 @@
-
+import React from 'react';
 import './App.css';
-
 import Ninjas from './Ninjas'
 
 function App() {
+  const state = {
+    ninjas: [
+      { name: 'viaksh', age: '22', path: 'red' },
+      { name: 'raja', age: '21', path: 'green' },
+      { name: 'bikash', age: '25', path: 'yellow' }
+    ]
+  }
   return (
     <div className="App">
       <h1>App</h1>
-      <Ninjas name="vikash" age="22" blet="bleack" />
+      <Ninjas list={state.ninjas} />
     </div>
   );
 }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default function Ninjas(props) {
-    const { name, age, blet } = props;
-    
-    return (
-        <div className="Ninjas">
-            <div>name:{name}</div>
+    const { list } = props;
+    const ListNinjas = list.map((item,i) =>{
+        return (<div key={i} className="listitem">
+            <div>name:{list.name}</div>
             <div>age:20</div>
             <div>Belt:)</div>
+            <hr />
+        </div>)
+    })
+    return (
+        <div className="Ninjas">
+            {ListNinjas}
         </div>
     )
 }
