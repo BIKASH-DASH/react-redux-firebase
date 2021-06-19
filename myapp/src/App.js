d4a441596eb01374507711b4a40bd4905d8efa70
biaksh-dash <vikashdash004@gmail.com>
2021-06-19 23:41:58 +0530

    passing data to parent to chaild

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <Ninjas/>
+      <Ninjas name="vikash" age="22" blet="bleack" />
     </div>
   );
 }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Ninjas() {
+export default function Ninjas(props) {
+    const { name, age, blet } = props;
+    
     return (
         <div className="Ninjas">
-            <div>name:vikash dash</div>
+            <div>name:{name}</div>
             <div>age:20</div>
             <div>Belt:)</div>
         </div>
