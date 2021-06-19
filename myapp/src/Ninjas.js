9282b46d7b5124c6d26e449838c599f59acad304
biaksh-dash <vikashdash004@gmail.com>
2021-06-19 23:36:31 +0530

    create root components

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Ninjas() {
+    return (
+        <div className="Ninjas">
+            <div>name:vikash dash</div>
+            <div>age:20</div>
+            <div>Belt:)</div>
+        </div>
+    )
+}
